c7112e26cd2dcbb12c75ea26fffb8b281bbcfa88
MICHKOWA <michal.b.kowalski@capgemini.com>
2015-11-09 12:57:30 +0100

    Pythagorean - working

--- src/main/java/com/capgemini/pythagorean/Pythagorean.java
+++ src/main/java/com/capgemini/pythagorean/Pythagorean.java
@@ -1,11 +1,35 @@
 package com.capgemini.pythagorean;
 
 /**
- * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
- * a2 + b2 = c2
- * For example, 32 + 42 = 9 + 16 = 25 = 52.
- * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
- * Find the product abc.
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for
+ * which, a2 + b2 = c2 For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2. There exists
+ * exactly one Pythagorean triplet for which a + b + c = 1000. Find the product
+ * abc.
  */
 public class Pythagorean {
+	// a < b < c, and natural numbers
+	// if a=0, then b=c (because a^2+b^2=c^2)
+	private static final int MIN_A = 1;
+	private static final int MIN_B = 2;
+	private static final int MIN_C = 3;
+
+	public static int[] findTriple(int sum) {
+		for (int c = MIN_C; c < sum; c++) {
+			for (int b = MIN_B; b < c; b++) {
+				for (int a = MIN_A; a < b; a++) {
+					if (a + b + c == sum) {
+						if (areTriple(a, b, c)) {
+							return new int[] { a, b, c };
+						}
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+
+	private static boolean areTriple(int a, int b, int c) {
+		return (a * a) + (b * b) == (c * c);
+	}
 }
---
+++ src/main/java/com/capgemini/pythagorean/PythagoreanTest.java
@@ -0,0 +1,41 @@
+package com.capgemini.pythagorean;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PythagoreanTest {
+
+	@Test
+	public void shouldReturnNullFor0() {
+		// given
+		int sum = 0;
+		// when
+		int actuals[] = Pythagorean.findTriple(sum);
+		// then
+		assertTrue(actuals == null);
+	}
+
+	@Test
+	public void shouldWorkFor12() {
+		// given
+		int sum = 12;
+		int expecteds[] = new int[] { 3, 4, 5 };
+		// when
+		int actuals[] = Pythagorean.findTriple(sum);
+		// then
+		assertArrayEquals(expecteds, actuals);
+	}
+
+	@Test
+	public void shouldWorkFor1000() {
+		// given
+		int sum = 1000;
+		int expecteds[] = { 200, 375, 425 };
+		// when
+		int actuals[] = Pythagorean.findTriple(sum);
+		// then
+		assertTrue(actuals != null);
+		assertArrayEquals(expecteds, actuals);
+	}
+}

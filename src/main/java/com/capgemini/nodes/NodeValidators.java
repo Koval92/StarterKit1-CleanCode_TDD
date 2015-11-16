1022a0b39b3afa0f6eb043ee4434e193b4c37715
MICHKOWA <michal.b.kowalski@capgemini.com>
2015-11-16 10:35:50 +0100

    NodeValidators - cleaning

--- src/main/java/com/capgemini/nodes/NodeValidators.java
+++ src/main/java/com/capgemini/nodes/NodeValidators.java
@@ -29,90 +29,137 @@ public class NodeValidators {
 	public static final String IS_ROOT = "ROOT";
 	public static final String INCONSISTENT_NODES = "Inconsistent";
 	public static final String TOO_MANY_CHILDREN = "Too many children or at wrong level";
-	private static final int ID_MAX_LENGTH = 4;
-	private static final int INITIAL_ROOT_COLOR = -1;
-	private static final int DESCRIPTION_MAX_LENGTH = 128;
 	public static final String NULL_PARAM = "One of params is null";
 
-	public static void validateMethod(List<Node> nodes) throws Exception {
-		Map<String, Integer> colorMap = new HashMap<>();
-		Map<String, List<String>> directChildren = new HashMap<>();
-		int rootColor = INITIAL_ROOT_COLOR;
+	private static final int ID_MAX_LENGTH = 4;
+	private static final int DESCRIPTION_MAX_LENGTH = 128;
+	private static final int INITIAL_ROOT_COLOR = -1;
 
+	private static Map<String, Integer> colorMap;
+	private static Map<String, List<String>> directChildren;
+	private static int rootColor;
+	private static List<Node> nodes;
+
+	public static void validateMethod(List<Node> nodesToValidate) throws Exception {
+		initializeFields(nodesToValidate);
+		basicCheckAndColorMapAndRootColorInitialization();
+		checkIfRootExists();
+		mergeColorMapAndCheckPredecessorCorrectness();
+		checkColorMapConsistency();
+		fillDirectChildrenMap();
+		checkNumberOfChildren();
+	}
+
+	private static void basicCheckAndColorMapAndRootColorInitialization() throws Exception {
 		for (int i = 0; i < nodes.size(); i++) {
 			Node currentNode = nodes.get(i);
 
 			checkForNulls(currentNode);
 			checkIdLength(currentNode);
 			checkDescriptionLength(currentNode);
-			
-			// check, if root ID already exists
-			if (colorMap.get(currentNode.getId()) != null) {
-				throw new Exception(DUPLICATED_ID);
-			}
-			colorMap.put(currentNode.getId(), i);
-			
-			// find root, and if only only exists
-			if (currentNode.getPredecessorId().equals(IS_ROOT)) {
-				if (rootColor != INITIAL_ROOT_COLOR) { // was changed before, so another root exists
-					throw new Exception(INCONSISTENT_NODES);
+			fillInitialColorMapForThisNodeAndCheckIfIsRoot(currentNode, i);
+		}
+	}
+
+	private static void checkNumberOfChildren() throws Exception {
+		for (Entry<String, List<String>> childrenEntry : directChildren.entrySet()) {
+			checkIfHasMoreThanTwoChildren(childrenEntry);
+			checkIfHasTwoChildrenAndIsPenultimate(childrenEntry);
+		}
+	}
+
+	private static void checkIfHasMoreThanTwoChildren(Entry<String, List<String>> childrenEntry) throws Exception {
+		if (childrenEntry.getValue().size() > 2) {
+			throw new Exception(TOO_MANY_CHILDREN);
+		}
+	}
+
+	private static void checkIfHasTwoChildrenAndIsPenultimate(Entry<String, List<String>> childrenEntry)
+			throws Exception {
+		if (childrenEntry.getValue().size() == 2) {
+			for (String childId : childrenEntry.getValue()) {
+				if (directChildren.get(childId) != null) {
+					throw new Exception(TOO_MANY_CHILDREN);
 				}
-				rootColor = i;
 			}
 		}
+	}
 
-		if (rootColor == -1) {
-			throw new Exception(NO_ROOT);
-		}
-
-		// color every node (and its children) by color of its parent
+	private static void fillDirectChildrenMap() {
 		for (Node node : nodes) {
-			int currentColor = colorMap.get(node.getId());
-			if (currentColor != rootColor) {
-				Integer newColor = colorMap.get(node.getPredecessorId());
-				if (newColor == null) { // predecessor not found in color map
-					throw new Exception(NOT_EXISTING_PREDECESSOR);
+			String predId = node.getPredecessorId();
+
+			if (!predId.equals(IS_ROOT)) {
+				if (!directChildren.containsKey(predId)) {
+					directChildren.put(predId, new ArrayList<>());
 				}
-				for (Map.Entry<String, Integer> entry : colorMap.entrySet()) {
-					if (entry.getValue().equals(currentColor)) {
-						entry.setValue(newColor);
-					}
-				}
+				directChildren.get(predId).add(node.getId());
 			}
 		}
+	}
 
+	private static void checkColorMapConsistency() throws Exception {
 		// check, if every node is connected to root
 		for (Map.Entry<String, Integer> entry : colorMap.entrySet()) {
 			if (!entry.getValue().equals(rootColor)) {
 				throw new Exception(INCONSISTENT_NODES);
 			}
 		}
-		
+	}
+
+	private static void mergeColorMapAndCheckPredecessorCorrectness() throws Exception {
 		for (Node node : nodes) {
-			String predId = node.getPredecessorId();
-			
-			if(!predId.equals(IS_ROOT)) {
-				if(!directChildren.containsKey(predId)) {
-					directChildren.put(predId, new ArrayList<>());
-				}
-				directChildren.get(predId).add(node.getId());
+			int currentColor = colorMap.get(node.getId());
+			if (currentColor != rootColor) {
+				int newColor = getColorOfPredecessor(node);
+				changeColorOfChildren(currentColor, newColor);
 			}
 		}
-		
-		// check number of children
-		for (Entry<String, List<String>> childrenEntry : directChildren.entrySet()) {
-			if(childrenEntry.getValue().size() > 2) {
-				throw new Exception(TOO_MANY_CHILDREN);
+	}
+
+	private static Integer getColorOfPredecessor(Node node) throws Exception {
+		Integer newColor = colorMap.get(node.getPredecessorId());
+		if (newColor == null) { // predecessor not found in color map
+			throw new Exception(NOT_EXISTING_PREDECESSOR);
+		}
+		return newColor;
+	}
+
+	private static void changeColorOfChildren(int currentChildColor, int newChildColor) {
+		for (Map.Entry<String, Integer> entry : colorMap.entrySet()) {
+			if (entry.getValue().equals(currentChildColor)) {
+				entry.setValue(newChildColor);
 			}
-			if(childrenEntry.getValue().size() == 2) {
-				System.out.println(childrenEntry.getKey());
-				for (String childId : childrenEntry.getValue()) {
-					System.out.println("\t" + childId);
-					if(directChildren.get(childId) != null) {
-						throw new Exception(TOO_MANY_CHILDREN);
-					}
-				}
+		}
+	}
+
+	private static void initializeFields(List<Node> nodesToInitialize) {
+		nodes = nodesToInitialize;
+		colorMap = new HashMap<>();
+		directChildren = new HashMap<>();
+		rootColor = INITIAL_ROOT_COLOR;
+	}
+
+	private static void checkIfRootExists() throws Exception {
+		if (rootColor == -1) {
+			throw new Exception(NO_ROOT);
+		}
+	}
+
+	private static void fillInitialColorMapForThisNodeAndCheckIfIsRoot(Node currentNode, int currentColor)
+			throws Exception {
+
+		if (colorMap.get(currentNode.getId()) != null) {
+			throw new Exception(DUPLICATED_ID);
+		}
+		colorMap.put(currentNode.getId(), currentColor);
+
+		if (currentNode.getPredecessorId().equals(IS_ROOT)) {
+			if (rootColor != INITIAL_ROOT_COLOR) { // was changed before, so
+													// another root exists
+				throw new Exception(INCONSISTENT_NODES);
 			}
+			rootColor = currentColor;
 		}
 	}
 
